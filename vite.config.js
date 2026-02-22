aca8fead9684dcc1fb3a163a971452c247a775f4
Bruno Lippert <brunolippert2002@gmail.com>
2026-02-22 15:39:28 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/tailwindcss-ui/", // Adicione esta linha com o nome exato do seu repositório
+  plugins: [react(), tailwindcss()],
 });
